72d83a93e4ece64af2399ffdb82b6968ef886661
BillAndyQ <billandyqm@gmail.com>
2026-09-19 14:45:10 -0500

    Initial commit

---
+++ restore.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+echo "Esperando PostgreSQL..."
+
+until docker exec postgres_db pg_isready -U root -d postgres > /dev/null 2>&1; do
+    sleep 2
+done
+
+echo "PostgreSQL está listo."
+
+echo "Restaurando backup..."
+
+cat backup_postgres.dump | docker exec -i postgres_db pg_restore \
+    -U root \
+    -d postgres \
+    --clean \
+    --if-exists \
+    --no-owner
+
+echo "Backup restaurado correctamente."
